8134fe0b54a2a02f26e4dc44fa21ef9b20768de5
Łukasz Myśliński <lukmyslinski@gmail.com>
2021-09-24 16:30:59 +0200

    Tried completing the oauth flow test but let's leave that for later

--- src/test/kotlin/io/prhunter/api/auth/OAuthTest.kt
+++ src/test/kotlin/io/prhunter/api/auth/OAuthTest.kt
@@ -5,16 +5,18 @@ import com.github.tomakehurst.wiremock.client.WireMock.*
 import io.prhunter.api.OAuthContextInitializer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import java.net.URLDecoder
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = [OAuthContextInitializer::class])
@@ -27,6 +29,9 @@ class OAuthTest(
     @Autowired
     private lateinit var wireMockServer: WireMockServer
 
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
+
     @LocalServerPort
     private val serverPort = 0
 
@@ -35,7 +40,7 @@ class OAuthTest(
         wireMockServer.resetAll()
     }
 
-    private fun stubUserInfoResponse(){
+    private fun stubUserInfoResponse() {
         wireMockServer.stubFor(
             get(urlPathMatching("/userinfo"))
                 .willReturn(
@@ -43,19 +48,13 @@ class OAuthTest(
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody(
-
-                            ("{\"sub\":\"111\"" +
-                                    ",\"login\":\"Mark Hoogenboom\"" +
-                                    ",\"created_at\":\"2008-01-14T04:33:35Z\"" +
-                                    ",\"id\":123" +
-                                    ", \"email\":\"mark.hoogenboom@example.com\"" +
-                                    "}")
+                            ("""{"sub":"111","login":"Mark Hoogenboom","created_at":"2008-01-14T04:33:35Z","id":123, "email":"mark.hoogenboom@example.com"}""")
                         )
                 )
         )
     }
 
-    private fun stubTokenResponse(){
+    private fun stubTokenResponse() {
         wireMockServer.stubFor(
             post(urlPathMatching("/oauth/token"))
                 .willReturn(
@@ -63,17 +62,28 @@ class OAuthTest(
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody(
-                            "{\"access_token\":\"my-access-token\"" +
-                                    ", \"token_type\":\"Bearer\"" +
-                                    ", \"expires_in\":\"3600\"" +
-                                    "}"
+                            """{"access_token":"my-access-token", "token_type":"Bearer", "expires_in":"3600"}"""
+                        )
+                )
+        )
+    }
+
+    fun stubLoginSuccessWithRedirect(state: String){
+        wireMockServer.stubFor(
+            get(urlPathMatching("/login/oauth/authorize"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(302)
+                        .withHeader(
+                            "Location",
+                            "/login/oauth2/code/github?code=invalid&state=${state}"
                         )
                 )
         )
     }
 
     @Test
-    fun `should fill in custom state when oauth triggered from install`(){
+    fun `should fill in custom state when oauth triggered from install`() {
         stubTokenResponse()
         stubUserInfoResponse()
         mockMvc.get("/login/oauth2/code/github?code=github&setup_action=install").andExpect {
@@ -82,43 +92,34 @@ class OAuthTest(
         }
     }
 
-    @Test()
-    @Disabled
-    fun stopOAuthResponses() {
-        wireMockServer.stubFor(
-            get(urlPathMatching("/oauth/authorize.*"))
-                .willReturn(
-                    aResponse()
-                        .withStatus(302)
-                        .withHeader(
-                            "Location",
-                            "http://localhost:${serverPort}/login/oauth2/code/my-oauth-client?code=my-acccess-code&state=\${state}"
-                        )
-                        .withTransformers("CaptureStateTransformer")
-                )
-        )
-        wireMockServer.stubFor(
-            post(urlPathMatching("/oauth/token"))
-                .willReturn(
-                    aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(
-                            "{\"access_token\":\"my-access-token\"" +
-                                    ", \"token_type\":\"Bearer\"" +
-                                    ", \"expires_in\":\"3600\"" +
-                                    "}"
-                        )
-                )
-        )
+    @Test
+    fun `should sign in with oauth successfully`(){
+        stubTokenResponse()
+        stubUserInfoResponse()
 
-//        // TODO this has to be done differently
-//        val redirectResult = mockMvc.get("/oauth2/authorization/github").andExpect {
-//            status { is3xxRedirection() }
-//        }.andReturn()
-//        val target = redirectResult.response.getHeader("Location")!!
-//        mockMvc.get(target).andExpect {
-//
+        // Login with github button clicked
+        val resp = mockMvc.get("/oauth2/authorization/github").andExpect {
+            status { is3xxRedirection() }
+        }.andReturn()
+        // Web client is redirected to the github site
+        val redirectUrl = URLDecoder.decode(resp.response.redirectedUrl)
+        val state = redirectUrl.split('&')[3].substring(6)
+        // github site calls our callback from the login
+
+        stubLoginSuccessWithRedirect(state)
+
+        val githubResponse = restTemplate.getForEntity<String>(redirectUrl)
+        val redirectLocation: String = githubResponse.headers["Location"]!![0]
+
+        Assertions.assertEquals(302, githubResponse.statusCode.value())
+        Assertions.assertEquals("/login/oauth2/code/github?code=invalid&state=${state}", redirectLocation)
+
+        // TODO finishing this test requires persisting the session between resttemplate and mockmvc... this is bit too much effort for now
+        val omg = mockMvc.get(redirectLocation).andReturn()
+//           omg.response
+//            .andExpect {
+//            status { is2xxSuccessful() }
+//            redirectedUrl("https://prhunter.io/signup-success")
 //        }
     }
 }
