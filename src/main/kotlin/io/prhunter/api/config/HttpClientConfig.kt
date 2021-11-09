58cdcfbde48bbfcff8cafd5a29b5035c58a7a301
Łukasz Myśliński <lukmyslinski@gmail.com>
2021-11-09 15:19:43 +0100

    Configured http client timeout

--- src/main/kotlin/io/prhunter/api/config/HttpClientConfig.kt
+++ src/main/kotlin/io/prhunter/api/config/HttpClientConfig.kt
@@ -2,6 +2,7 @@ package io.prhunter.api.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.client.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.logging.*
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,10 +19,13 @@ class HttpClientConfig(
         return HttpClient {
             install(Logging) {
                 logger = Logger.SIMPLE
-                level = LogLevel.NONE
+                level = LogLevel.ALL
             }
             install(JsonFeature){
                 serializer = JacksonSerializer(objectMapper)
+            }
+            install(HttpTimeout){
+                requestTimeoutMillis = 10000
             }
         }
     }
