196011d2ea9a8efc72742d70683a561ae38df259
Łukasz Myśliński <lukmyslinski@gmail.com>
2021-11-09 15:04:48 +0100

    Corrected auth token type casting

--- src/main/kotlin/io/prhunter/api/RequestUtil.kt
+++ src/main/kotlin/io/prhunter/api/RequestUtil.kt
@@ -1,10 +1,10 @@
 package io.prhunter.api
 
 import io.prhunter.api.user.GithubUser
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import java.security.Principal
 
 object RequestUtil {
     fun getUserFromRequest(principal: Principal): GithubUser =
-        ((principal as OAuth2AuthenticationToken).principal as GithubUser)
+        ((principal as AbstractAuthenticationToken).principal as GithubUser)
 }
