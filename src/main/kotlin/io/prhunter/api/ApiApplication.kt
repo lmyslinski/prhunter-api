673033454f5de39dd1c02cacc70f57b0b29400b5
Łukasz Myśliński <lukmyslinski@gmail.com>
2021-12-03 00:07:39 +0100

    JWT refreshing that actually works

--- src/main/kotlin/io/prhunter/api/ApiApplication.kt
+++ src/main/kotlin/io/prhunter/api/ApiApplication.kt
@@ -3,8 +3,10 @@ package io.prhunter.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ApiApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/io/prhunter/api/github/GithubAuthServiceImpl.kt
+++ src/main/kotlin/io/prhunter/api/github/GithubAuthServiceImpl.kt
@@ -1,14 +1,11 @@
 package io.prhunter.api.github
 
-import com.github.benmanes.caffeine.cache.Cache
-import com.github.benmanes.caffeine.cache.Caffeine
 import mu.KotlinLogging
 import org.kohsuke.github.GHApp
 import org.kohsuke.github.GitHubBuilder
 import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import java.util.concurrent.TimeUnit
-import javax.annotation.PostConstruct
 
 private val log = KotlinLogging.logger {}
 
@@ -18,29 +15,23 @@ class GithubAuthServiceImpl(
     private val githubSecrets: GithubSecrets
 ) : GithubAuthService {
 
-    companion object{
-        private const val APP_KEY = "PRHUNTER"
+    private var ghApp: GHApp? = null
+
+    companion object {
+        // every 5 minutes
+        const val REFRESH_INTERNAL: Long = (1000 * 60 * 5).toLong()
     }
 
-    @PostConstruct
-    private fun initJwtCache(){
-        cache.put(APP_KEY, getApplicationGithubClient())
-    }
-
-    // We need to regenerate the application JWT every 5 minutes since max JWT expiration is 10 minutes
-    private val cache: Cache<String, GHApp> = Caffeine.newBuilder()
-        .refreshAfterWrite(5, TimeUnit.MINUTES)
-        .build { getApplicationGithubClient() }
-
-    private fun getApplicationGithubClient(): GHApp {
+    @Scheduled(fixedRate = REFRESH_INTERNAL)
+    private fun getApplicationGithubClient() {
         log.info { "Refreshing application JWT key" }
         val tmpPrivateKey = GithubJwtService.generateTmpPrivateKey(githubSecrets.privateKey)
         val jwtToken = GithubJwtService.generateJwtKey(githubSecrets.appId, tmpPrivateKey)
-        return GitHubBuilder().withJwtToken(jwtToken).build().app
+        ghApp = GitHubBuilder().withJwtToken(jwtToken).build().app
     }
 
     override fun getInstallationAuthToken(installationId: Long): String {
-        val appInstallation = cache.getIfPresent(APP_KEY)!!.getInstallationById(installationId)
+        val appInstallation = ghApp!!.getInstallationById(installationId)
         return appInstallation.createToken().create().token
     }
 
