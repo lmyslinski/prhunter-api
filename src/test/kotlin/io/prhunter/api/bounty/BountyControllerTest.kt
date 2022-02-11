5ecc571ca73a10ee058f376192e5fb062d3a7c1f
Łukasz Myśliński <lukmyslinski@gmail.com>
2022-02-11 13:37:52 +0100

    Removed config from project

--- src/test/kotlin/io/prhunter/api/bounty/BountyControllerTest.kt
+++ src/test/kotlin/io/prhunter/api/bounty/BountyControllerTest.kt
@@ -145,7 +145,7 @@ class BountyControllerTest(
             )
         )
 
-        val response = mockMvc.post("/bounty") {
+        mockMvc.post("/bounty") {
             content = objectMapper.writeValueAsString(createBountyRequest)
             contentType = MediaType.APPLICATION_JSON
             accept = MediaType.APPLICATION_JSON
