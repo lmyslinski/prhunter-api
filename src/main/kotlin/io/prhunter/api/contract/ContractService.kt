60bffeaf70670bc8407a15d8ccebfb8110971769
Łukasz Myśliński <lukmyslinski@gmail.com>
2022-02-23 15:16:12 +0100

    Extracted common contract code

--- src/main/kotlin/io/prhunter/api/contract/ContractService.kt
+++ src/main/kotlin/io/prhunter/api/contract/ContractService.kt
@@ -1,8 +1,69 @@
 package io.prhunter.api.contract
 
-interface ContractService{
+import io.prhunter.api.bounty.BountyRepository
+import io.prhunter.api.bounty.BountyStatus
+import io.prhunter.api.github.GithubAppService
+import mu.KotlinLogging
+import org.web3j.crypto.Credentials
+import org.web3j.protocol.Web3j
+import org.web3j.tx.exceptions.ContractCallException
 
-    fun checkPendingContracts()
+abstract class ContractService(
+    private val bountyRepository: BountyRepository,
+    private val githubAppService: GithubAppService,
+    private val lazyGasProvider: LazyGasProvider,
+) {
 
-    fun payoutBounty(targetAddress: String, bounty: io.prhunter.api.bounty.Bounty)
+    protected abstract val bountyFactory: BountyFactory
+    protected abstract val web3j: Web3j
+    protected abstract val credentials: Credentials
+
+    protected val log = KotlinLogging.logger {}
+
+    fun checkPendingContracts() {
+        val pendingBounties = bountyRepository.findAllByBountyStatus(BountyStatus.PENDING)
+        pendingBounties.forEach { bounty ->
+            val bountyAddressOpt = bountyFactory.allBounties(bounty.id.toString()).send()
+            if (bountyAddressOpt != null && bountyAddressOpt != "0x0000000000000000000000000000000000000000") {
+                val contractId = getContractBountyId(bountyAddressOpt)
+                if (contractId == bounty.id.toString()) {
+                    activateBounty(bounty, bountyAddressOpt)
+                }
+            }
+        }
+    }
+
+    fun payoutBounty(targetAddress: String, bounty: io.prhunter.api.bounty.Bounty) {
+        try {
+            val address = bountyFactory.allBounties(bounty.id.toString()).send()
+            val bountyContract = Bounty.load(address, web3j, credentials, lazyGasProvider)
+            bountyContract.payoutBounty(targetAddress).send()
+            // TODO verify the TX was successful
+            // maybe add the TX in an event so that we can monitor what's going on
+            log.info { "Successfully submitted a payout bounty tx from bounty $address to $targetAddress" }
+        } catch (ex: Throwable) {
+            log.error(ex) { "Fatal error, could not load bounty in order to payout bounty" }
+        }
+    }
+
+    private fun getContractBountyId(bountyAddressOpt: String): String? {
+        return try {
+            Bounty.load(bountyAddressOpt, web3j, credentials, lazyGasProvider).bountyId().send()
+        } catch (ex: ContractCallException) {
+            log.warn { "Could not load contract at $bountyAddressOpt" }
+            null
+        }
+    }
+
+    private fun activateBounty(bounty: io.prhunter.api.bounty.Bounty, bountyAddress: String) {
+        log.info { "Bounty ${bounty.id} deployed successfully, activating" }
+        try {
+            bounty.bountyStatus = BountyStatus.ACTIVE
+            bounty.blockchainAddress = bountyAddress
+            bountyRepository.save(bounty)
+            githubAppService.newBountyComment(bounty)
+        } catch (ex: Throwable) {
+            log.error(ex) { "An error was occurred while activating bounty ${bounty.id}" }
+        }
+    }
 }
--- src/main/kotlin/io/prhunter/api/contract/EthContractService.kt
+++ src/main/kotlin/io/prhunter/api/contract/EthContractService.kt
@@ -15,69 +15,24 @@ import org.web3j.tx.exceptions.ContractCallException
 @Service
 @Profile("!test")
 class EthContractService(
-    private val bountyRepository: BountyRepository,
-    private val githubAppService: GithubAppService,
-    private val lazyGasProvider: LazyGasProvider,
+    bountyRepository: BountyRepository,
+    githubAppService: GithubAppService,
+    lazyGasProvider: LazyGasProvider,
     @Value("\${crypto.alchemyUrl}") val alchemyUrl: String,
     @Value("\${crypto.ethPkey}") val ethPrivateKey: String,
     @Value("\${crypto.bountyFactoryEthAddress}") val bountyFactoryEthAddress: String
-) : ContractService {
+) : ContractService(bountyRepository, githubAppService, lazyGasProvider) {
 
-    private val log = KotlinLogging.logger {}
-    private val web3j = Web3j.build(HttpService(alchemyUrl))
-    private val credentials = Credentials.create(ethPrivateKey)
-    private val bountyFactory: BountyFactory = BountyFactory.load(
+    // TODO extract chain info into separate bean that's passed around
+    // so that we can inject it as required (ex. for comments and urls)
+
+    final override val web3j = Web3j.build(HttpService(alchemyUrl))
+    final override val credentials = Credentials.create(ethPrivateKey)
+    final override val bountyFactory: BountyFactory = BountyFactory.load(
         bountyFactoryEthAddress,
         web3j,
         credentials,
         lazyGasProvider
     )
 
-    @Suppress("UNCHECKED_CAST")
-    override fun checkPendingContracts() {
-        val pendingBounties = bountyRepository.findAllByBountyStatus(BountyStatus.PENDING)
-        pendingBounties.forEach { bounty ->
-            val bountyAddressOpt = bountyFactory.allBounties(bounty.id.toString()).send()
-            if (bountyAddressOpt != null && bountyAddressOpt != "0x0000000000000000000000000000000000000000") {
-                val contractId = getContractBountyId(bountyAddressOpt)
-                if (contractId == bounty.id.toString()) {
-                    activateBounty(bounty, bountyAddressOpt)
-                }
-            }
-        }
-    }
-
-    private fun activateBounty(bounty: io.prhunter.api.bounty.Bounty, bountyAddress: String) {
-        log.info { "Bounty ${bounty.id} deployed successfully, activating" }
-        try{
-            bounty.bountyStatus = BountyStatus.ACTIVE
-            bounty.blockchainAddress = bountyAddress
-            bountyRepository.save(bounty)
-            githubAppService.newBountyComment(bounty)
-        }catch (ex: Throwable){
-            log.error(ex) { "An error was occurred while activating bounty ${bounty.id}" }
-        }
-    }
-
-    override fun payoutBounty(targetAddress: String, bounty: io.prhunter.api.bounty.Bounty) {
-        try {
-            val address = bountyFactory.allBounties(bounty.id.toString()).send()
-            val bountyContract = Bounty.load(address, web3j, credentials, lazyGasProvider)
-            bountyContract.payoutBounty(targetAddress).send()
-            // TODO verify the TX was successful
-            // maybe add the TX in an event so that we can monitor what's going on
-            log.info { "Successfully submitted a payout bounty tx from bounty $address to $targetAddress" }
-        } catch (ex: Throwable) {
-            log.error(ex) { "Fatal error, could not load bounty in order to payout bounty" }
-        }
-    }
-
-    private fun getContractBountyId(bountyAddressOpt: String): String? {
-        return try {
-            Bounty.load(bountyAddressOpt, web3j, credentials, lazyGasProvider).bountyId().send()
-        } catch (ex: ContractCallException) {
-            log.warn { "Could not load contract at $bountyAddressOpt" }
-            null
-        }
-    }
 }
